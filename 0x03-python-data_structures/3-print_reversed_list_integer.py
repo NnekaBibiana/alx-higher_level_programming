1c8ecf3dead124f91e21dea92538a79e7e99ca02
nneka <okoronnekajuliet@gmail.com>
2022-12-06 20:11:21 -0800

    tuplis

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
-st_integer(my_list=[]): 
+#!/usr/bin/python3 
+ def print_reversed_list_integer(my_list=[]): 
      if my_list: 
          my_list.reverse() 
          for i in range(len(my_list)): 
              print("{:d}".format(my_list[i]))
-
